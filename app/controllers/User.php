7f60c5b4cfc2643acd8459a09c0a1a9f1385f633
ThePope05 <swobben05@gmail.com>
2023-12-04 20:04:19 +0100

    Added mvc for user

---
+++ app/controllers/User.php
@@ -0,0 +1,13 @@
+<?php
+
+class User extends BaseController
+{
+    public function index()
+    {
+        $data = [
+            'title' => 'User'
+        ];
+
+        $this->view('User/index', $data);
+    }
+}
