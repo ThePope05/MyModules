bf8aa3cfd541f8fcc8c337df1e79816d79c93759
ThePope05 <swobben05@gmail.com>
2023-12-05 11:05:52 +0100

    Created table users create script

---
+++ app/db/Create_Table_Users.sql
@@ -0,0 +1,8 @@
+USE ProjectName;
+
+CREATE TABLE Users(
+	Id tinyint primary key,
+    Email varchar(150) not null unique key,
+    Name varchar(30) not null,
+    Password varchar(255) not null
+)Engine=Innodb;
